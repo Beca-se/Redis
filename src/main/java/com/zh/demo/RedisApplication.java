5b30b0bd1d7bbc7eddc0fb7df6bdd72fb6c81733
zhouhu <15756874962@163.com>
2019-12-18 01:07:53 +0800

    [Desc]: 搭建基础环境

---
+++ src/main/java/com/zh/demo/RedisApplication.java
@@ -0,0 +1,17 @@
+package com.zh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouhu
+ * @Class Name RedisApplication
+ * @Desc 主启动类
+ * @create: 2019-12-18 01:03
+ **/
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
